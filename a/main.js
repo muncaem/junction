eedcf4a169891c4ab55ffef38cde293cad5eda1b
Jeongyoujeong <happyou10@naver.com>
2022-08-21 11:17:28 +0900

    trash 수정

--- a/main.js
+++ a/main.js
@@ -1,7 +1,15 @@
 // load sprite
-let trash = App.loadSpritesheet('trash.png', 48, 43, 0, 1);
-let tr = 0;
+let trash = App.loadSpritesheet('trash.png', 48, 43, 0, 10);
 let _stateTimer = 0;
+
+const STATE_INIT = 3000;
+const STATE_READY = 3001;
+const STATE_PLAYING = 3002;
+const STATE_JUDGE = 3004;
+const STATE_END = 3005;
+
+let _state = STATE_INIT;
+
 
 function increase() {
     let x = Math.random() * 80; 
@@ -14,23 +22,26 @@ App.onUpdate.Add(function(dt) {
 
     _stateTimer += dt;
 
-    for(let i = 0; i < 11; i++)
-    {
-        if(_stateTimer >= 2) {
-            App.showCenterLabel("The game will start soon.");}
-        if(_stateTimer >= 4) {
-            App.showCenterLabel("Pick Up the Trash");}
-        if(6 < _stateTimer < 10) {
-            App.showCenterLabel("Pick Up the Trash");
-            increase();
-        }
-    
-        if(_stateTimer == 10) {
+    switch(_state) {
+        case STATE_INIT:
+            if(_stateTimer >= 2) {
+                App.showCenterLabel("The game will start soon.");
+            }
+            if(_stateTimer >= 4)
+                App.showCenterLabel("Pick Up the Trash");
+            if(_stateTimer >= 6)
+                increase();
+            break;
+        case STATE_PLAYING: 
+            break;
+        case STATE_JUDGE:
             judgement();
-        }
-    }
-
+            break;
+        case STATE_END:
+            break;
+    };
 });
+
 
 function judgement() {
     if(trash == null) {
@@ -43,5 +54,5 @@ function judgement() {
 
 // 플레이어가 오브젝트를 공격(Z키)했을 때 호출 되는 이벤트
 App.onObjectAttacked.Add(function(sender, x, y) {
-    Map.putObject(x, y, trash) == null;
+    Map.clearAllObjects();
 });
